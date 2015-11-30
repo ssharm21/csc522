9bc5e36471fc3f26a15afbc72d590d4be30b627e
SHIVANI SHARMA <shivanidev2015@gmail.com>
2015-11-29 21:14:42 -0500

    Fixed run.py

--- run.py
+++ run.py
@@ -1,12 +1,58 @@
 import pickle
 import cv2
+import os
+import pandas as pd
 import stasm
 from itertools import combinations
 from scipy.spatial import distance
 import numpy as np
 from sklearn import preprocessing
+import math
+from sklearn.decomposition import PCA
+from sklearn import svm
 
 model = pickle.load(open("rbf_model.bin", "rb"))
+
+emotions = {}
+for root, dirs, files in os.walk("Emotion/"):
+    path = root.split('/')
+    for _file in files:
+        text = open(("/").join(path) + "/" + _file, "r").read()
+        name = _file.split("_")[0] + "_" + _file.split("_")[1]
+        emotions[name] = int(text.split(".")[0])
+
+
+res_dict = pd.read_csv("compare_data.csv")
+
+np_points = res_dict.select_dtypes(exclude=["object", "int64"])
+
+np_points = np_points.as_matrix()
+np_points = np.nan_to_num(np_points)
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(np_points)
+
+
+
+pca = PCA(n_components=5)
+pca.fit(x_scaled)
+pca_features = pca.transform(x_scaled)
+
+
+X = []
+Y = []
+
+for i in range(1, 8):
+	for n in res_dict["name"]:
+		if n in emotions.keys() and emotions[n] == i:
+			ind = np.where(res_dict["name"]==n)[0][0]
+			X.append(pca_features[ind])
+			Y.append(i)
+
+
+
+classifier = svm.SVC(kernel="rbf", gamma=0.08)
+classifier.fit(X, Y) 
+
 
 img_neutral = cv2.imread("test/neutral.png", cv2.IMREAD_GRAYSCALE)
 img_emotion = cv2.imread("test/emotion.png", cv2.IMREAD_GRAYSCALE)
@@ -43,5 +89,6 @@ x_scaled = min_max_scaler.fit_transform(np_points)
 
 pca = pickle.load(open("pca.bin", "rb"))
 pca_features = pca.transform(x_scaled)
-print pca_features
-print model.predict(pca_features)
+labels = {1: "anger", 2: "neutral", 3: "disgust", 4: "fear", 5: "happiness", 6: "sadness", 7:"surprise"}
+print "DETECTED EMOTION:"
+print labels[classifier.predict(pca_features)[0]]
